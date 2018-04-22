76d79be08ce2164e37967be065ac1388a27b3897
wang0nya <kwangonya@gmail.com>
2018-04-22 11:48:23 +0300

    edit html, cleanup dead code

--- lib/app_component.dart
+++ lib/app_component.dart
@@ -1,8 +1,6 @@
 import 'package:angular/angular.dart';
 import 'package:angular_components/angular_components.dart';
 
-import 'src/todo_list/todo_list_component.dart';
-
 // AngularDart info: https://webdev.dartlang.org/angular
 // Components info: https://webdev.dartlang.org/components
 
@@ -10,9 +8,8 @@ import 'src/todo_list/todo_list_component.dart';
   selector: 'my-app',
   styleUrls: const ['app_component.css'],
   templateUrl: 'app_component.html',
-  directives: const [materialDirectives, TodoListComponent],
+  directives: const [materialDirectives],
   providers: const [materialProviders],
 )
 class AppComponent {
-  // Nothing here yet. All logic is in TodoListComponent.
 }
